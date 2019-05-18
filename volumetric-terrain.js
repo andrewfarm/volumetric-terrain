ea0cdc73b9c83544e3aa4eb53ab0bc3a0d407b12
andrewfarm <Andrew.Farm@tufts.edu>
2019-05-18 19:17:13 -0400

    Render cube with three.js

---
+++ volumetric-terrain.js
@@ -0,0 +1,42 @@
+var scene;
+var camera;
+var renderer;
+
+var geometry;
+var material;
+var cube;
+
+var simplex = new SimplexNoise();
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    geometry = new THREE.BoxGeometry(1, 1, 1);
+    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+if (WEBGL.isWebGLAvailable()) {
+    init();
+    animate();
+} else {
+    var warning = WEBGL.getWebGLErrorMessage();
+    document.body.appendChild(warning);
+}
